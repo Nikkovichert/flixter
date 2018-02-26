84280140fb636c54ba8b880e8ca550214898c9c8
Nikko <nikkovichert@gmail.com>
2018-02-26 11:12:38 +0000

    finished product3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-//= require jquery-ui
+//= require popper
+//= require tether
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery-ui
+//= require_tree .
